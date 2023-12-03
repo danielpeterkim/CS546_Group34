56ce108557599a72d74144f7003d5642c0b9ce0b
Diego <123908767+diego-developed@users.noreply.github.com>
2023-12-03 18:58:14 -0500

    Add files via upload
    Template file structure - not functional yet

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+const app = express();
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const staticDir = express.static(__dirname + '/public');
+
+app.use('/public', staticDir);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log('Your routes will be running on http://localhost:3000');
+});
