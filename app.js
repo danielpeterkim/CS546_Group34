4cee9e7d659e25169ef54f43872a842825d754c5
Diego <123908767+diego-developed@users.noreply.github.com>
2023-12-05 17:57:27 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 import express from 'express';
+import session from 'express-session';
+import exphbs from 'express-handlebars';
+import configRoutes from './routes/index.js';
+
 const app = express();
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
-const staticDir = express.static(__dirname + '/public');
-
-app.use('/public', staticDir);
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+app.use(session({
+  name: 'AuthState',
+  secret: 'some secret string!',
+  resave: false,
+  saveUninitialized: false
+}));
+
+
+configRoutes(app);
 
 app.listen(3000, () => {
   console.log("We've now got a server!");
   console.log('Your routes will be running on http://localhost:3000');
 });
+
