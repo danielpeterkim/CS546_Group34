5cec9039159dd435832c04cffbf0c831bef0397e
durnazzer <danplaysgamestoo@gmail.com>
2023-12-17 18:24:51 -0500

    fixed middleware

--- app.js
+++ app.js
@@ -28,11 +28,8 @@ app.use((req, res, next) => {
   if (!req.session.player && (route !== '/login' && route !== '/register')) {
       return res.redirect('/login');
   }
-  if (req.session.player && (route === '/pvp/battleprep' || route === '/pvp/battleresults') && !req.session.inCombat) {
-      return res.redirect('/city');
-  }
   //better way to make middleware according to w3schools
-  if (req.session.player && !['/city', '/logout', '/buy-building', '/destroy-building', '/get-player', '/pvp', 'report', 'report-player', '/pvp/targeted-battle', '/pvp/random-attack', '/pvp/execute-battle', '/tasks'].includes(route)) {
+  if (req.session.player && !['/city', '/logout', '/buy-building', '/destroy-building', '/get-player', '/pvp', '/report', '/report-player', '/pvp/targeted-battle', '/pvp/random-attack', '/pvp/execute-battle', '/tasks'].includes(route)) {
       return res.redirect('/city');
   }
 
